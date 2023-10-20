7904d1fed7f949897e1532d577aec578dcfa59e8
Steven Zheng <121006797+stzheng716@users.noreply.github.com>
2023-10-20 16:15:21 -0700

    add models file from the draw.io models (#12)
    * add models file from the draw.io models
    
    set up the app.py file
    
    * update readme and requirements.txt

---
+++ models.py
@@ -0,0 +1,79 @@
+"""SQLAlchemy models for Web Scraper."""
+
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import URLType
+
+
+db = SQLAlchemy()
+
+class JobBoards(db.Model):
+    """Job Boards Pages."""
+
+    __tablename__='job_boards'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    company_name = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    careers_url = db.Column(
+        URLType,
+        nullable=False
+    )
+
+    career_date_scraped = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
+
+class JobPostings(db.Model):
+    """Job postings from job boards"""
+
+    __tablename__='job_postings'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    job_title = db.Column(
+        db.String()
+    )
+
+    job_url = db.Column(
+        URLType,
+        nullable=False
+    )
+
+    job_scraped_date = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
+
+    company = db.Column(
+        db.Integer,
+        db.ForeignKey('job_boards.id', ondelete='CASCADE'),
+        nullable=False
+    )
+
+    JSON_response = db.Column(
+        db.JSON
+    )
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
