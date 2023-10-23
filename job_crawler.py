b375b8053161b9bbbb58937e3a246cc82c67fdce
Crynowecer0 <crynowecer11@gmail.com>
2023-10-23 14:21:27 -0400

    chore: remove currently unused imports

--- job_crawler.py
+++ job_crawler.py
@@ -1,7 +1,5 @@
 import time
-import csv
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
