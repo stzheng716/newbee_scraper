d5e1cf69da5e1d9dae422c3834f1f155e1b355ac
Camran Rynowecer <Crynowecer11@gmail.com>
2023-10-23 22:11:22 -0400

    16 tier 1 scraper add ats url field to database (#17)
    * feat!: update bulk_insert
    
    -added extract_domain helper method
    -running bulk insert will now also add data to the proper field
    
    * feat: add ats_url field to JobBoards
    
    * chore: update readme to flex current state of applicationion

--- bulk_insert.py
+++ bulk_insert.py
@@ -2,6 +2,7 @@
 import psycopg2
 import os
 import csv
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,6 +16,30 @@ cursor = conn.cursor()
 table_name = 'job_boards'
 columns = ('company_name', 'careers_url')
 
+def extract_domain(url):
+    """extracts the domain from an url
+
+    params:
+    - a url string
+
+    returns:
+    - the domain portion of a url
+
+    ex:
+    "https://www.example.com/path/to/page" -> "example.com"
+
+    """
+    # Parse the URL
+    parsed_url = urlparse(url)
+
+    # Get the hostname (domain) portion from the parsed URL
+    domain = parsed_url.hostname
+
+    # Check if the domain starts with "www." and remove it if present
+    if domain and domain.startswith("www."):
+        domain = domain[4:]  # Remove "www."
+
+    return domain
 
 #insert csv from scrape into database, checking for duplicates
 with open('hrefs.csv', 'r') as f:
@@ -23,13 +48,14 @@ with open('hrefs.csv', 'r') as f:
     for row in csv_reader:
         company_name = row[0]
         careers_url = row[1]
+        ats_url = extract_domain(careers_url)
 
         insert_query = f"""
-                INSERT INTO {table_name} (company_name, careers_url)
-                VALUES (%s, %s)
+                INSERT INTO {table_name} (company_name, careers_url, ats_url)
+                VALUES (%s, %s, %s)
                 ON CONFLICT (company_name) DO NOTHING;
                 """
-        cursor.execute(insert_query, (company_name, careers_url))
+        cursor.execute(insert_query, (company_name, careers_url, ats_url))
 
 conn.commit
 conn.close()
