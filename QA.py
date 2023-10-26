d4f827c389c11c0a538b6ac9b09a7cc4f9bb06f5
Michael <mherman315@gmail.com>
2023-10-26 15:58:48 -0700

    GH qa:
    Greenhouse QA is returning data as expected

--- QA.py
+++ QA.py
@@ -1,9 +1,17 @@
 from utils import sql_url_query
 from ashby_scraper import scrape_ashby_job_board
+from greenhouse_scraper import scrape_greenhouse_job_board
 
-def test_scrapers():
+def test_ashby():
     ats_dict = sql_url_query()
     for url in ats_dict["ashby"]:
         scrape_ashby_job_board(url.careers_url)
+        print("JOBURL---->", url.careers_url)
+        breakpoint()
+        
+def test_greenhouse():
+    ats_dict = sql_url_query()
+    for url in ats_dict["greenhouse"]:
+        scrape_greenhouse_job_board(url.careers_url)
         print("JOBURL---->", url.careers_url)
         breakpoint()
