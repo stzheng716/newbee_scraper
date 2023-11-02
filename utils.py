ec168de786834020c2ca6c88b3244817224fa6de
Steven <stzheng716@gmail.com>
2023-11-01 20:51:11 -0700

    adjust the database connections
    change the psycopg2 connection from the user local database to the AWS database for bulk_insert.py and utils.py

--- utils.py
+++ utils.py
@@ -9,8 +9,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#connect to the database directly for the insert
-conn = psycopg2.connect(database=os.environ["DATABASE_NAME"])
+#connect to the local database directly for the insert
+# conn = psycopg2.connect(database=os.environ["DATABASE_NAME"])
+
+#connect to the AWS database directly for the insert
+conn = psycopg2.connect(
+    dbname=os.environ["DATABASE_NAME"],
+    user=os.environ["RDS_USERNAME"],
+    password=os.environ["RDS_PW"],
+    host=os.environ["AWS_DATABASE_URL_EP"]
+)
+
 conn.autocommit = True
 cursor = conn.cursor()
 
