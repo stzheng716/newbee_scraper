b33a6fbe9004d65c95a5a899c2dbd63a3ec2df9b
Michael Herman <113940622+MGHermanMancarella@users.noreply.github.com>
2023-11-29 14:24:35 -0800

    103 filter gpt bug (#115)
    * minor Readme adjustment and print statement additions to GPT
    
    * updated backup sql file with current data (11.22.23)
    
    * changed some function names. Not married to them, just thought it'd clarify some stuff (and add a little fun)
    
    * updated sql db. fixed GPT bug
    
    * pull
    
    * utils.py fn names:
    
    More uniform naming schema
    Changed names across various files
    Separated db queries in the file
    
    * utils.py fn names:
    
    More uniform naming schema
    Changed names across various files
    Separated db queries in the file
    updated readme
    Restructured files: data contains only db backups and variables (headers.py) that we draw on in the rest of the code. Utils now holds all utility functions
    
    * corrected import file path names (leftover from branch 102)
    
    * reverted back to original patter: scrape all job postings and filter them in the gpt function
    
    * full_scrape: corrected import filepath
    
    * filter_GPT.py: corrected import filepath name
    
    * wrapped the t3 scrape in a try/except block.
    Adjusted query for t3 to only pass job_postings without a job_description value
    
    * merge main

--- t3/job_description_scraper.py
+++ t3/job_description_scraper.py
@@ -1,7 +1,7 @@
 import requests
 import time
 from bs4 import BeautifulSoup
-from utilities.utils import query_unblessed_US_jobs
+from utilities.db_utils import query_all_job_posting
 from utilities.db_bulk_data_utils import bulk_insert_jds
 
 
@@ -14,45 +14,44 @@ def scrape_job_description(url):
     response = None
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Check if the request was successful
+        soup = BeautifulSoup(response.content, 'html.parser')
+        job_description = ""
+
+        if "greenhouse" in url:
+            content_divs = soup.find_all('div', id='content')
+            if content_divs:  # Check if the list is not empty
+                job_description = content_divs[0].text.replace(
+                    "\n", " ").strip()
+            else:
+                print("T3 >>> No div with id='content' found.")
+        elif "ashby" in url:
+            job_description = soup.find('meta', attrs={'name': 'description'}).get(
+                'content').replace("\n", "").strip()
+        elif "lever" in url:
+            job_line = soup.find_all('div', class_="section page-centered")
+            texts = [div.text for div in job_line]
+            job_description = '\n'.join(texts).replace('\n', "")
+
+        # print(job_description)
+        return job_description
     except Exception as e:
         print("t3: Error processing HTML content: ", e)
-        return "null"
-
-    soup = BeautifulSoup(response.content, 'html.parser')
-    job_description = ""
-
-    if "greenhouse" in url:
-        content_divs = soup.find_all('div', id='content')
-        if content_divs:  # Check if the list is not empty
-            job_description = content_divs[0].text.replace(
-                "\n", " ").strip()
-        else:
-            print("T3 >>> No div with id='content' found.")
-    elif "ashby" in url:
-        job_description = soup.find('meta', attrs={'name': 'description'}).get(
-            'content').replace("\n", "").strip()
-    elif "lever" in url:
-        job_line = soup.find_all('div', class_="section page-centered")
-        texts = [div.text for div in job_line]
-        job_description = '\n'.join(texts).replace('\n', "")
-
-    # print(job_description)
-    return job_description
+        return "scrape error"
 
 
 
 def aggregate_job_descriptions():
     '''
-    This function sends the URL off to scrape_j_d() and returns a JD, 
-    then appends it to a tuple that is then added to a large list. 
+    This function sends the URL off to scrape_j_d() that returns a JD, 
+    then it appends it to a tuple with its associated job_id. 
+    That is then added to a large list. 
     The list of tuples is used to bulk insert the data into the DB
     
     returns job_descriptions = 
         [( 'job description string', 'job_id'), ()...]
         '''
     job_descriptions = []
-    for job in query_unblessed_US_jobs():
+    for job in query_all_job_posting():
         jd_text = scrape_job_description(job[0]).strip()
         job_descriptions.append((jd_text, job[1]))
         print("t3 job description length >>> ", len(job_descriptions))
